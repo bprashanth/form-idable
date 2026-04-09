c023f79a54b4dd4e4898c0c958f47386ad241d56
Prashanth.B <prashanth@tech4goodcommunity.com>
2026-04-09 14:30:33 +0530

    Fixed local deployment to avoid SW worker
    The SW worker caches unnecessary assets and results in all forms of
    weird whiteouts. It is overall unnecessary, as this app requires
    internet to function. Remove it.
    
    Also documents setup and phasing in `pwa.md`.

--- pwa/src/main.js
+++ pwa/src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
-import { registerSW } from 'virtual:pwa-register'
 import App from './App.vue'
 import router from './router.js'
 import { useCognitoAuth } from './composables/useCognitoAuth.js'
 import './main.css'
-
-registerSW({ immediate: true })
 
 const { init } = useCognitoAuth()
 init().then(() => {
